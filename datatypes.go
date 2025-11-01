fac36df26b0d6b660241d4fba2b0b54de24967c9
efranken-25 <efranken@andrew.cmu.edu>
2025-10-31 22:26:13 -0400

    add file

---
+++ datatypes.go
@@ -0,0 +1,18 @@
+//Noemi Banda, Emma Franken, Beth Vazquez Smith
+//Programming for Scientists Project
+//October 31, 2025
+
+package main
+
+type ExpressionMatrix [][]float64 //or would this be map[string]float64 
+
+// using Network instead of tree
+// Network is slice of pointers to nodes
+type Network []*Node
+
+type Node struct {
+	ExpressionVal  *float64  //strength of co-expression
+	Weights        []float64 //need weight for weighted matrix
+	GeneName       string
+	Child1, Child2 *Node //need to figure out a way to not have finite num of children (i think its Children []*Node)
+}
