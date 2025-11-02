4db62dd412ea5f355f433ac28ff4718177c935e4
efranken-25 <efranken@andrew.cmu.edu>
2025-11-02 17:37:19 -0500

    wrote pearson

--- datatypes.go
+++ datatypes.go
@@ -4,15 +4,15 @@
 
 package main
 
-type ExpressionMatrix [][]float64 //or would this be map[string]float64
+type ExpressionMatrix map[string]float64 //upgma was [][]float64
 
 // using Network instead of tree
 // Network is slice of pointers to nodes
 type Network []*Node
 
 type Node struct {
-	ExpressionVal  *float64  //strength of co-expression
-	Weights        []float64 //need weight for weighted matrix
-	GeneName       string
-	Child1, Child2 *Node //need to figure out a way to not have finite num of children (i think its Children []*Node)
+	ExpressionVal *float64  //strength of co-expression
+	Weights       []float64 //need weight for weighted matrix
+	GeneName      string
+	Children      []*Node //need to figure out a way to not have finite num of children (i think its Children []*Node)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module 02-601_Project_Fall2025
 
 go 1.25.1
 
-
+require gonum.org/v1/gonum v0.16.0 // indirect
